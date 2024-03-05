4a2511097ea1fb9b0ad91b40ad2241c3a944753d
Ruth Chisom <ndukwe100805043@gmail.com>
2024-03-05 14:52:53 +0100

    2D Arrays - answer sheet

--- 2d_arrays.js
+++ 2d_arrays.js
@@ -1,3 +1,4 @@
+// QUESTION SHEET
 // Declare a 2-dimensional array, arr, of n empty arrays. All arrays are zero indexed.
 // Declare an integer, lastAnswer, and initialize it to 0.
 // There are 2 types of queries, given as an array of strings for you to parse:
@@ -63,3 +64,65 @@
 // arr[0] = [5, 3] 
 // arr[1] = [7]
 // 3
+
+
+// ANSWER SHEET
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function dynamicArray(n, queries) {
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const q = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(q);
+
+    for (let i = 0; i < q; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = dynamicArray(n, queries);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
