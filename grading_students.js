8d6e48c63630f688b6e33bc1b4ecafa13cd95f85
Ruth Chisom <ndukwe100805043@gmail.com>
2024-11-26 14:30:01 +0100

    Grading Students - My Answer

--- grading_students.js
+++ grading_students.js
@@ -45,3 +45,79 @@
 // 4. Student 4 received a grade below 33, so the grade will not be modified and the student's final grade is 33.
 
 // ANSWER SHEET
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+    // Write your code here
+    console.log(grades)
+    for(let i=0; i<grades.length; i++){
+        if(grades[i]>=38){
+            let remainder = grades[i] % 5;//4
+            if(remainder>=3){
+                let extraMark = 5 - remainder;
+                grades[i] += extraMark;
+                console.log(grades[i]);
+            }
+        }
+    }
+    // grades.forEach((element)=>{
+    //     if(element>40){
+    //         let remainder = element % 5;//4
+    //         if(remainder>=3){
+    //             let extraMark = 5 - remainder;
+    //             element += extraMark;
+    //             console.log(element);
+    //         }
+    //     }
+    // })
+return grades;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
+
